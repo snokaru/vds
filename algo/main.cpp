7fcdcf42400b0ed2c9b7d2108d45224456640629
Vlad Slivilescu <vlad.slivilescu00@e-uvt.ro>
2021-11-26 19:04:40 +0200

    Initial Commit

---
+++ algo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algo
+//
+//  Created by Vlad Slivilescu on 26.11.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
